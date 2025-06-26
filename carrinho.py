8ab19e464de62a13e8548b36e09d27fca134352a
Verônica <veronica9120@gmail.com>
2025-06-26 09:33:42 -0300

    Adiciona solução do desafio carrinho de compras

---
+++ carrinho.py
@@ -0,0 +1,29 @@
+# Lista para armazenar os produtos e preços
+carrinho = []
+total = 0.0
+
+# Entrada do número de itens
+n = int(input().strip())
+
+# Loop para adicionar itens ao carrinho
+for _ in range(n):
+    linha = input().strip()
+    
+    # Encontra a última ocorrência de espaço para separar nome e preço
+    posicao_espaco = linha.rfind(" ")
+    
+    # Separa o nome do produto e o preço
+    item = linha[:posicao_espaco]
+    preco = float(linha[posicao_espaco + 1:])
+    
+    # Adiciona ao carrinho
+    carrinho.append((item, preco))
+    total += preco
+
+for item, preco in carrinho:
+  print(f'{item}: R${preco:.2f}')
+  
+print(f'Total: R${total:.2f}')
+  
+
+  
